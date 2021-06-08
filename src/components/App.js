4f51e9ce3bf9520c27641518ee5567721e607f5b
Kevin Jiménez <ji.oso.kev@gmail.com>
2021-06-07 20:36:16 -0500

    Finalizando el repaso de react y empezando con redux

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Menu from './Menu';
+import Usuarios from './Usuarios/index';
+
+const Tareas = () => <div>Tareas</div>;
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Menu />
+      <Route exact path='/' component={Usuarios} />
+      <Route exact path='/tareas' component={Tareas} />
+    </BrowserRouter>
+  );
+};
+
+export default App;
